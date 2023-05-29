9aff3b2a35c7514431b88c63652aad3e6826b458
Maame Yaa Kesewaa Omari <117319465+Kesewaa-Omari@users.noreply.github.com>
2023-05-29 14:25:41 +0000

    functions.c

--- functions.c
+++ functions.c
@@ -130,7 +130,7 @@ int print_int(va_list types, char buffer[], int flags,
 	if (n == 0)
 	buffer[i--] = '0';
 
-	buffer[i--] = '\0';
+	buffer[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
 
 	if (n < 0)
